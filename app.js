dc0808a0bc421d7f6227bd7d69b00c032be0e1f3
mustafasuhateber <msteber2506@gmail.com>
2024-05-25 15:24:17 -0400

    Added a database model and connected to database

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const express = require("express");
-const app = express();
-const path = require('path');
+const path = require("path");
+const mongoose = require("mongoose");
+const Restaurant = require('./models/restaurant')
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.get("/", (req, res) => {
-  res.render('home');
+mongoose.connect("mongodb://localhost:27017/halal-eats", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+const app = express();
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/restaurants", async (req, res) =>{
+  const restaurants = await Restaurant.find({});
+  res.render('restaurants/index', {restaurants});
+})
+
 app.listen(3000, () => {
-  console.log('LISTENING ON PORT 3000!');
+  console.log("LISTENING ON PORT 3000!");
 });
