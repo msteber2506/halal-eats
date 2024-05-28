1da35fc94458e103655bc149fc7dbd4d9572e876
root <root@Alp>
2024-05-28 13:20:06 -0400

    added css files and created homepage commented code lines that not working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const Restaurant = require("./models/restaurant");
 
 mongoose.connect("mongodb://localhost:27017/halal-eats", {
@@ -14,7 +15,7 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("Database connected");
 });
-
+app.engine("ejs", ejsMate)
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
