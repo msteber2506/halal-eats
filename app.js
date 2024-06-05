1035f336ed3ee2ebb7d2d50a631c5af9208fd217
Alp-Usta <salperen01@icloud.com>
2024-06-05 18:48:46 -0400

    added edit and update features still needs to be checked

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const Restaurant = require("./models/restaurant");
+const { resolveSoa } = require("dns");
 
 mongoose.connect("mongodb://localhost:27017/halal-eats", {
   useUnifiedTopology: true,
+  useNewUrlParser: true
 });
 
 const app = express();
@@ -15,8 +17,11 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({extended : true}))
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -27,9 +32,24 @@ app.get("/restaurants", async (req, res) => {
   res.render("restaurants/index", { restaurants });
 });
 
+app.get("/restaurants/new", (req, res) => {
+  res.render("restaurants/new");
+})
+
+app.post("/restaurants",async(req, res) =>{
+  const restaurant = new Restaurant(req.body.restaurant);
+  await restaurant.save();
+  res.redirect(`/restaurants/${restaurant._id}`)
+})
+
 app.get("/restaurants/:id", async (req, res) => {
   const restaurant = await Restaurant.findById(req.params.id);
   res.render("restaurants/show", { restaurant });
+});
+
+app.get("/restaurants/:id/edit", async(req, res) => {
+  const restaurant = await Restaurant.findById(req.params.id)
+  res.render("restaurants/edit", { restaurant });
 });
 
 app.listen(3000, () => {
